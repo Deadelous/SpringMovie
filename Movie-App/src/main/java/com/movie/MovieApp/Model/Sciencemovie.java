47bfdd512580c71c9a5adff92213bbcad01d70d7
Leo Hetterscheid <Testichon@gmail.com>
2018-05-30 23:23:55 +0200

    Add a Repository package
    Package with repositories

--- Movie-App/src/main/java/com/movie/MovieApp/Model/Sciencemovie.java
+++ Movie-App/src/main/java/com/movie/MovieApp/Model/Sciencemovie.java
@@ -3,6 +3,7 @@ package com.movie.MovieApp.Model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.io.Serializable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "sciencemovies")
--- Movie-App/src/main/java/com/movie/MovieApp/MovieAppApplication.java
+++ Movie-App/src/main/java/com/movie/MovieApp/MovieAppApplication.java
@@ -2,8 +2,10 @@ package com.movie.MovieApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
